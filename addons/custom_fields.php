ef8fd9aea2cb7c97992213d04a589b385a492bd4
sumitramanga <sumitramanga@gmail.com>
2018-11-09 11:14:12 +1300

    Created staff role types and date to display on the page along with security.

--- addons/custom_fields.php
+++ addons/custom_fields.php
@@ -40,16 +40,22 @@
 		global $metaboxes;
 		$fields = $metaboxes[$args['id']]['fields'];
 
+		// keeps input value within the field.
+		$customValues = get_post_custom($post->ID);
+
+		$output = '<input type="hidden" name="post_format_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'">';
+
+
 		if (! empty($fields)) {
 			foreach ($fields as $id => $field) {
 				switch($field['type']){
 					case 'text':
 						$output .= '<label for="'.$id.'">'.$field['title'].'</label>';
-						$output .= '<input type="text" name="'.$id.'" class="" style="width:100%">';
+						$output .= '<input type="text" name="'.$id.'" class="" value="'.$customValues[$id][0].'">';
 					break;
 					case 'number':
 						$output .= '<label for="'.$id.'">'.$field['title'].'</label>';
-						$output .= '<input type="number" name="'.$id.'" style="width:100%">';
+						$output .= '<input type="number" name="'.$id.'"  value="'.$customValues[$id][0].'">';
 					break;
 					case 'select':
 						$output .= '<label for="'.$id.'">'.$field['title'].'</label>';
@@ -64,3 +70,46 @@
 		}
 		echo $output;
 	}
+
+// Makign custom field data save
+
+function save_metaboxes($postID) {
+	global $metaboxes;
+
+	if(! wp_verify_nonce( $_POST['post_format_meta_box_nonce'], basename(__FILE__) )) {
+		return $postID;
+	}
+
+	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
+		return $postID;
+	}
+
+	if ($_POST['post_type'] == 'page') {
+		if( ! curent_user_can()('edit_page', $postID)){
+			return $postID;
+		}
+	} else if ( ! current_user_can('edit_page', $postID)) {
+		return $postID;
+	}
+
+	$post_type = get_post_type();
+
+	foreach ($metaboxes as $id => $metabox) {
+		if( $metabox['applicableto'] == $post_type){
+			$fields = $metaboxes[$id]['fields'];
+
+			foreach ($fields as $id => $field) {
+				$oldValue = get_post_meta($postID, $id, true);
+				$newValue = $_POST[$id];
+
+				if ($newValue && $newValue != $oldValue) {
+					update_post_meta($postID, $id, $newValue);
+				} elseif ($newValue == '' && $oldValue || !isset($_POST[$id])) {
+					delete_post_meta($postID, $id, $oldValue);
+				}
+			}
+		}
+	}
+}
+
+add_action('save_post', 'save_metaboxes');
