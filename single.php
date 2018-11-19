d9ce5db2a0fd6708ce669b433edb1fed352810c1
sumitramanga <sumitramanga@gmail.com>
2018-11-20 09:51:57 +1300

    Displayed comments and comments form. Also changed posts per page for the staff posts in the previous commit

--- single.php
+++ single.php
@@ -8,7 +8,31 @@
 
 		<!-- shows the content template -->
 		<?php get_template_part('content', get_post_format()); ?>
-		
+
+		<div class="container">
+			<div class="col">
+				<?php $comments_args = array(
+					'comment_field' => '<div class="form-group">
+											<label for="comment">Enter your comment</label>
+											<textarea name="comment" class="form-control" rows=10 aria-required="true"></textarea>
+										</div>'
+				) ?>
+				<?php comment_form($comments_args, get_the_id()); ?>
+			</div>
+			<div class="col">
+				<?php
+					$comments = get_comments(array(
+						'post_id' => get_the_ID(),
+						'status' => 'approve'
+					));
+					wp_list_comments('', $comments);
+
+				 ?>
+			</div>
+		</div>
+
+
+
 	<?php endwhile; ?>
 <?php endif; ?>
 
